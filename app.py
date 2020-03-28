ea10965c100db1da2b58209d05b670e1f82717fa
Dylan Snelgrove <dsnelgro@uwaterloo.ca>
2020-03-28 14:54:04 -0400

    graphs and such

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import mpld3
+import graphs_test
 
+app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    return graphs_test.get_data()
 
--- graphs_test.py
+++ graphs_test.py
@@ -3,11 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import mpld3
 
-df = pd.DataFrame({
-    "Skill": [9, 9, 11],
-    "Ages": [18, 19, 19],
-    "Goals": [2, 4, 3]
-})
+def get_data():
+    df = pd.DataFrame({
+        "Skill": [9, 9, 11],
+        "Ages": [18, 19, 19],
+        "Goals": [2, 4, 3]
+    })
 
-plt.plot(df)
-mpld3.fig_to_dict()
+    plt.plot(df)
+    fig, _ = plt.subplots()
+    return mpld3.fig_to_dict(fig)
