2c4c2eb686c607a6f18640ce22cd66857ff6211a
Li-Evelyn <lievelynpl@gmail.com>
2020-03-28 19:36:56 -0400

    useless things finally work

--- asdf.py
+++ asdf.py
@@ -2,11 +2,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from basic_tests import dorchester_arr
 
-testing = pd.read_csv("Miovision Data/Dorchester Road and Huron Church Road_tmc_data_test.csv")[["dt_bin", "Direction", "Movement Type",
-                                                                                  "Vehicle Classification",
-                                                                                  "Number of Vehicles"]]
-print(dorchester_arr[0])
+testing = pd.read_csv("Miovision Data/Dorchester Road and Huron Church Road_tmc_data_test.csv")[["dt_bin", "Direction",
+                                                                                                 "Movement Type",
+                                                                                                 "Vehicle Classification",
+                                                                                                 "Number of Vehicles"]]
+placeholder = ["Right", "Thru", "Left", "U-Turn"]
+for i in range(1, 4):
+    for j in range(0, 4):
+        placeholder.append(placeholder[j] + "." + str(i))
 
+dfs = [pd.DataFrame({placeholder[i]: dorchester_arr[j][placeholder[i]] for i in range(len(placeholder))}) for j in range(len(dorchester_arr))]
+sum_arr = [0 for i in range(len(dorchester_arr))]
+shift = 5764
+
+for j in range(len(dfs)):
+    for header in placeholder:
+        for i in range(0 + shift * j, len(dfs[j]["Right"]) + shift * j):
+            sum_arr[j] += dfs[j][header][i]
+
+print(sum_arr)
 
 vehicles_aggregate = testing[["Vehicle Classification", "Number of Vehicles"]]
 
@@ -24,4 +38,4 @@ for i in range(0, len(vehicles_aggregate)):
     totals[vehicles_aggregate["Vehicle Classification"][i]] += vehicles_aggregate["Number of Vehicles"][i]
 
 
-#print(totals)
+print(totals)
