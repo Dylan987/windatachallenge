3e9e9ebdae22b474c971ddee4b80df8278268c59
Dylan Snelgrove <dsnelgro@uwaterloo.ca>
2020-03-29 00:42:48 -0400

    idek:

--- static/scripts/site.js
+++ static/scripts/site.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
     // the state
     let current_street = 0;
-    
+    let current_direction = 0;
+    let current_vehicle = 0;
     
     // add the event listeners
     const update_graph = function(){
+        // call the AJAX method with the given params 
+    };
+
+    const process_street_button = function() {
         
     };
 });
