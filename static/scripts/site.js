d870064eeadfd035af4409ca07e09f59c78cc5cf
Dylan Snelgrove <dsnelgro@uwaterloo.ca>
2020-03-29 00:39:06 -0400

    fixed site.html

---
+++ static/scripts/site.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // the state
+    let current_street = 0;
+    
+    
+    // add the event listeners
+    const update_graph = function(){
+        
+    };
+});
+
